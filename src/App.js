0ec364a8cb2e3851f7dce1d23d79164c2104c6af
Charles-Henry Noah <101463788+CharlesHenrynoah@users.noreply.github.com>
2024-12-28 21:08:00 +0000

    signup1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './components/theme';
 import Login from './components/Login';
 import Home from './components/Home';
+import SignupStep1 from './components/SignupStep1';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignupStep1 />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
